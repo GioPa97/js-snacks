77f029d8b6733dc02240761be50820db5c86ee7b
GioPa97 <giovannipagliara97@gmail.com>
2022-11-14 17:23:17 +0100

    Esercizio 3 finito

---
+++ jsnack3/script.js
@@ -0,0 +1,20 @@
+let somma = 0;
+
+for (i=1; i<=10; i++) {
+    let num = parseInt(prompt("Inserisci un numero"));
+    if(isNaN(num)) {
+        num = 0;
+    }
+    
+    somma += num;
+}
+
+console.log(
+`
+Somma: ${somma}
+
+`
+);
+
+
+
